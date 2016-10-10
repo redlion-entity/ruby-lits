6702fcaf86c0c0fc0c06d1bcc023a305fec550d6
redlion-entity <redlion.2016@ya.ru>
2016-10-10 09:44:40 +0300

    lesson15: Associated models

---
+++ hometasks/lesson15/twitter/app/models/twit.rb
@@ -0,0 +1,4 @@
+class Twit < ActiveRecord::Base
+  belongs_to :user
+  validates :content, presence: true
+end
---
+++ hometasks/lesson15/twitter/app/models/user.rb
@@ -0,0 +1,13 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
+class User < ActiveRecord::Base
+  has_many :twits, dependent: :destroy
+  validates :nickname, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 5}
+  validates :email, presence: true, uniqueness: true, email: true
+end
