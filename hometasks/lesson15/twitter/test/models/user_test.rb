8c444d5d1b145123fc1b1db9845c6ecd26c05262
redlion-entity <redlion.2016@ya.ru>
2016-10-26 18:10:27 +0300

    lesson19: typing some user unit tests

--- hometasks/lesson15/twitter/test/models/user_test.rb
+++ hometasks/lesson15/twitter/test/models/user_test.rb
@@ -1,17 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  # Tests "Create"
+  test "user is saved" do
+  	user = User.new(nickname: 'qweqwe', email: "qwe@qwe.qwe", avatar: 'https://robohash.org/marianne?size=36x36')
+  	assert user.save, "Saving the user with is full data"
   end
 
-  test "user is saved" do
-  	user = User.new(nickname: 'qweqwe', email: "qwe@qwe.qwe")
-  	assert user.save
+  test "should save user without avatar" do
+    user = User.new(nickname: 'qweqwe', email: "qwe@qwe.qwe")
+    assert user.save
+  end
+
+  test "should not save user with a blank initial data" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test "should not save user without nickname" do
+    user = User.new(email: "qwe@qwe.qwe", avatar: 'https://robohash.org/marianne?size=36x36')
+    assert_not user.save
   end
 
   test "should not save user without email" do
-    user = User.new(nickname: "asdasd")
+    user = User.new(nickname: 'qweqwe', avatar: 'https://robohash.org/marianne?size=36x36')
   	assert_not user.save
   end
 end
